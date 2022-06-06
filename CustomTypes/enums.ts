5dda2093e9a161869e3b63e37c656d3774e502f7
KaziMdMehediHasan <=>
2022-06-06 10:33:38 +0600

    enum concept added

---
+++ CustomTypes/enums.ts
@@ -0,0 +1,27 @@
+let petOnSale = 'chinchilla';
+let ordersArray = [
+  ['rat', 2], 
+  ['chinchilla', 1], 
+  ['hamster', 2], 
+  ['chinchilla', 50]
+];
+
+// Write your code below:
+
+enum Pet {
+  Hamster,
+  Rat,
+  Chinchilla,
+  Tarantula
+}
+
+let petOnSaleTS: Pet = Pet.Chinchilla;
+
+let ordersArrayTS: [Pet,number][] =[
+  [Pet.Rat,2],
+  [Pet.Chinchilla,1],
+  [Pet.Hamster,2],
+  [Pet.Chinchilla,50]
+]
+
+// ordersArrayTS.push([Pet.Jerboa, 3]); will not work as there is no Jerboa in the enum
